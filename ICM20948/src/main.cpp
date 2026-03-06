fe16ad2e2baabd86111cfbcdf0ed0cbc68dace4b
Kristoffer <mitloginergodt+GitHub@outlook.com>
2026-03-06 23:49:55 +0100

    get readings

--- ICM20948/src/main.cpp
+++ ICM20948/src/main.cpp
@@ -2,11 +2,17 @@
 #include "function.h"
 #include "ICM_20948.h"
 
+
 #define SERIAL_PORT Serial
 #define WIRE_PORT Wire
 #define AD0_VAL 1
 
 ICM_20948_I2C myICM;
+
+float accX, accY, accZ;
+float gyrX, gyrY, gyrZ;
+float magX, magY, magZ;
+
 
 void setup() {
   SERIAL_PORT.begin(115200);
@@ -31,10 +37,12 @@ void setup() {
       initialized = true;
     }
   }
-
-  int8_t temp = myICM.agmt.tmp.val();
+  while (!myICM.dataReady()) {}
+  myICM.getAGMT();
+  int8_t temp = myICM.temp();
   SERIAL_PORT.print("Temperature: ");
-  SERIAL_PORT.print(temp);
+  SERIAL_PORT.println(temp);
+  
 }
 
 void loop() {
@@ -42,6 +50,20 @@ void loop() {
   if (myICM.dataReady())
   {
     myICM.getAGMT();         
+    
+    accX = myICM.accX(); accY = myICM.accY(); accZ = myICM.accZ();
+    gyrX = myICM.gyrX(); gyrY = myICM.gyrY(); gyrZ = myICM.gyrZ();
+    magX = myICM.magX(); magY = myICM.magY(); magZ = myICM.magZ();
+    
+    SERIAL_PORT.print(accX); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(accY); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(accZ); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(gyrX); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(gyrY); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(gyrZ); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(magX); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(magY); SERIAL_PORT.print(", ");
+    SERIAL_PORT.print(magZ); SERIAL_PORT.println();
     
   }
   delay(100);
